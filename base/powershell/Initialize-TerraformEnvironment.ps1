3af44b50e4477f43d37476c8f0285499ed175c2c
unknown <smcolligan@gmail.com>
2018-10-14 21:10:48 -0500

    changed env var names and added allow source ip

--- base/powershell/Initialize-TerraformEnvironment.ps1
+++ base/powershell/Initialize-TerraformEnvironment.ps1
@@ -20,22 +20,36 @@ param (
   [Parameter(ParameterSetName='azure',Mandatory=$true, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
     [string]$ClientID,
   [Parameter(ParameterSetName='azure',Mandatory=$true, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
-    [string]$ClientSecret
+    [string]$ClientSecret,
+  [Parameter(Mandatory=$false, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+    [string]$AllowSourceIP
 )
 
 begin {
   $ErrorActionPreference = "Stop"
 
+  # set the value of allow source ip - prefer passed in value
+  if ($PSBoundParameters.ContainsKey('AllowSourceIP')) {
+    $localAllowSourceIP = $AllowSourceIP
+  }
+  else {
+    $localAllowSourceIP = (Invoke-RestMethod -Uri 'http://ipinfo.io/json' -Verbose:$false | Select-Object -ExpandProperty 'ip')
+  }
+
+  # set general terraform environment variables
+  Write-Verbose ("Setting Environment Variable `"{0}`" = `"{1}`"." -f 'TF_VAR_azure_tenant_id', $localAllowSourceIP)
+  $env:TF_VAR_allow_source_ip = $localAllowSourceIP
+
   # set azure terraform environment variables
   if ($Azure) {
-    Write-Verbose ("Setting Environment Variable `"{0}`" = `"{1}`"." -f 'TF_VAR_AzureTenantID', $TenantID)
-    $env:TF_VAR_AzureTenantID = $TenantID
-    Write-Verbose ("Setting Environment Variable `"{0}`" = `"{1}`"." -f 'TF_VAR_AzureSubscriptionID', $SubscriptionID)
-    $env:TF_VAR_AzureSubscriptionID = $SubscriptionID
-    Write-Verbose ("Setting Environment Variable `"{0}`" = `"{1}`"." -f 'TF_VAR_AzureClientID', $ClientID)
-    $env:TF_VAR_AzureClientID = $ClientID
-    Write-Verbose ("Setting Environment Variable `"{0}`" = `"{1}`"." -f 'TF_VAR_AzureClientSecret', $ClientSecret)
-    $env:TF_VAR_AzureClientSecret = $ClientSecret  
+    Write-Verbose ("Setting Environment Variable `"{0}`" = `"{1}`"." -f 'TF_VAR_azure_tenant_id', $TenantID)
+    $env:TF_VAR_azure_tenant_id = $TenantID
+    Write-Verbose ("Setting Environment Variable `"{0}`" = `"{1}`"." -f 'TF_VAR_azure_subscription_id', $SubscriptionID)
+    $env:TF_VAR_azure_subscription_id = $SubscriptionID
+    Write-Verbose ("Setting Environment Variable `"{0}`" = `"{1}`"." -f 'TF_VAR_azure_client_id', $ClientID)
+    $env:TF_VAR_azure_client_id = $ClientID
+    Write-Verbose ("Setting Environment Variable `"{0}`" = `"{1}`"." -f 'TF_VAR_azure_client_secret', $ClientSecret)
+    $env:TF_VAR_azure_client_secret = $ClientSecret  
   }
 }
 
